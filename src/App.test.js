fba76131cfcc5ea6b32fda094dce8ba69366a66d
seoyoung-kim-kr <seoyoung-kim-kr@gmail.com>
2023-04-23 18:15:45 +0900

    Setup project , Add TodoList component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+function App() {
+  return <div></div>;
+}
+
+export default App;
